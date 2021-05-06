8fc7504e8428dc747cf908595f897e15516c70a0
danroyal001 <danroyaleffiong@gmail.com>
2021-05-06 19:10:50 +0100

    average performance

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,7 +2,10 @@ import * as functions from "firebase-functions";
 import setCors from './util/setCors';
 import _ssr from './util/ssr';
 
-export const ssr = functions.https.onRequest(async (req, res) => {
+export const ssr = functions.runWith({
+  memory: '4GB',
+  timeoutSeconds: 540
+}).https.onRequest(async (req, res) => {
   const url = req.query.url as string;
 
   const html = await _ssr(url);
